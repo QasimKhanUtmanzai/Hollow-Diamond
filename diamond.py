8c10674b78d631596b5f7012955d2aa243fe3de3
QasimKhanUtmanzai <32437076+QasimKhanUtmanzai@users.noreply.github.com>
2020-06-28 19:04:28 +0200

    this is a code for hollow diamond

---
+++ diamond.py
@@ -0,0 +1,30 @@
+i=0
+j=0
+k=0
+for i in range(10,0,-1):
+    k=k+1
+
+    for j in range(10,0,-1):
+        if j==k:
+            print("*", end="")
+        else:
+            print("  ", end="")
+    for j in range(11):
+        if j==k:
+            print("*", end="")
+        else:
+            print("  ", end="")
+    print()
+for i in range(10):
+    k=k+1
+    for j in range(10):
+        if j==i:
+            print("*", end="")
+        else:
+            print("  ", end="")
+    for j in range(10,0,-1):
+        if j==i:
+            print("*", end="")
+        else:
+            print("  ", end="")
+    print()
